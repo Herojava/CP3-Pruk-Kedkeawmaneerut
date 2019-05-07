a158d39b4bad34c65df715be23375b0181354180
Herojava <lionman_1990@hotmail.com>
2019-05-07 21:49:35 +0700

    Create Exercise4_Pruk_K.py
    Exercise 4

---
+++ Exercise4_Pruk_K.py
@@ -0,0 +1,10 @@
+Foundation_English_Score = 99.9
+General_Business_Score = 98.8
+Introduction_to_Computer_Systems_Score = 97.7
+Computer_Programming_Score = 96.6
+
+print("--- Your Score ---")
+print("Foundation English :", Foundation_English_Score)
+print("General Business :", General_Business_Score)
+print("Introduction to Computer Systems :", Introduction_to_Computer_Systems_Score)
+print("Computer Programming :", Computer_Programming_Score)
