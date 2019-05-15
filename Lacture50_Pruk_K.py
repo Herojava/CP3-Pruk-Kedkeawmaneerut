0d9060d723b808b07d789c0a72eb6afd6754ddda
Herojava <lionman_1990@hotmail.com>
2019-05-15 20:54:41 +0700

    Create Lacture50_Pruk_K.py
    Lacture50

---
+++ Lacture50_Pruk_K.py
@@ -0,0 +1,18 @@
+def addNubber(x,y):
+    print(x+y)
+def minusNubber(x,y):
+    print(x-y)
+def divideNubber(x,y):
+    print(x/y)
+def mutiNubber(x,y):
+    print(x*y)
+
+print("--------------------------------------")
+addNubber(int(input("x :")),int(input("y :")))
+print("--------------------------------------")
+minusNubber(int(input("x :")),int(input("y :")))
+print("--------------------------------------")
+mutiNubber(int(input("x :")),int(input("y :")))
+print("--------------------------------------")
+divideNubber(int(input("x :")),int(input("y :")))
+print("--------------------------------------")
